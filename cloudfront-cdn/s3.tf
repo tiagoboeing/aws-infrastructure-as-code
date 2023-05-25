1e6f901b3d3863e3a1c2ab891fce01a8383892c6
Tiago Boeing <contato@tiagoboeing.com>
2023-05-25 18:11:44 -0300

    fix: :bug: Fixed issue related to `AccessControlListNotSupported` when creating `bucket_acl`

--- cloudfront-cdn/s3.tf
+++ cloudfront-cdn/s3.tf
@@ -10,6 +10,22 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_s3_bucket_acl" "bucket_acl" {
   bucket = aws_s3_bucket.bucket.id
   acl    = "private"
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership
+  ]
+}
+
+# Resource to avoid error "AccessControlListNotSupported: The bucket does not allow ACLs"
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+
+  depends_on = [
+    aws_s3_bucket.bucket
+  ]
 }
 
 resource "aws_s3_bucket_public_access_block" "bucket_public_access" {
